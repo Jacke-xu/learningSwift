84ed40728f12babe18127b14fca3cb20d99e60b9
786452470 <fyf786452470@gmail.com>
2018-08-05 16:36:11 +0800

    提交slider 和 label

--- swift4.2/frameworks(框架)/CocoaTouchLayer(触摸层)/UIKit/swift (alert and label)/swift (alert and label)/ViewController.swift
+++ swift4.2/frameworks(框架)/CocoaTouchLayer(触摸层)/UIKit/swift (alert and label)/swift (alert and label)/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testButton = UIButton(frame: CGRect(x: 100, y: 100, width: 80, height: 30))
+        testButton.center.x = self.view.center.x
+        testButton.backgroundColor = UIColor.black
+        testButton.addTarget(self, action: #selector(click(_:)), for: .touchUpInside)
+        self.view.addSubview(testButton)
+        
+        
+        
+        let slider = UISlider(frame: CGRect(x: 30, y: 200, width: self.view.frame.size.width - 60, height: 20))
+        slider.addTarget(self, action: #selector(sliderMoved(_:)), for: .touchUpInside)
+        slider.minimumTrackTintColor = UIColor.black
+        slider.maximumTrackTintColor = UIColor.red
+        self.view.addSubview(slider)
+        
+    }
+    
+    @objc func click(_ sender: UIButton) {
+        let message = "The value of the slider is : \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, world",
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @objc func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
