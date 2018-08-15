9ed1331971045651f761f11ad1ae8796381a5254
786452470 <fyf786452470@gmail.com>
2018-08-15 09:24:04 +0800

    初始化swift（关键字）总结

---
+++ swift4.2/frameworks(框架)/CoreServicesLayer(核心服务层)/Foundation/swift(关键字)/swift(关键字)/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  swift(关键字)
+//
+//  Created by 范云飞 on 2018/8/15.
+//  Copyright © 2018年 范云飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MARK:-------------------------- swift 词汇结构 -----------------------------
+        
+        //MARK:---------------- 在声明中使用的关键字 -----------------
+        //MARK:1. associatedtype
+        
+        //MARK:---------------- 在语句中使用的关键字 -----------------
+        //MARK:---------------- 在表达式和类型使用的关键字 ---------------
+        //MARK:---------------- 模式中使用的关键字 ------------------
+        //MARK:---------------- 以数字符号#开头的关键字 -----------------
+        //MARK:---------------- 在特定上下文中被保留的关键字 ------------
+        
+        
+        //FIXME:以下标记被当做保留符号，不能用于自定义操作符
+        /**
+         ( 、 ) 、 { 、 } 、 [ 、 ] 、 . 、 , 、 : 、 ; 、 = 、 @ 、 # 、 & （作为前缀操作符）、 -> 、 `  、 ? 和 ! (作为后缀操作符)
+         */
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
