ca5884cb089b89699d199a93204bd8d6c8eb1f6f
786452470 <fyf786452470@gmail.com>
2018-08-22 08:57:04 +0800

    创建swift(NSAttributedString)工程，提交iOS开发学习路线图

---
+++ swift4.2/frameworks(框架)/CoreServicesLayer(核心服务层)/Foundation/swift(NSAttributedString)/swift(NSAttributedString)/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  swift(NSAttributedString)
+//
+//  Created by 范云飞 on 2018/8/21.
+//  Copyright © 2018年 范云飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var attributedString = NSAttributedString(string: "去你大爷的")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
