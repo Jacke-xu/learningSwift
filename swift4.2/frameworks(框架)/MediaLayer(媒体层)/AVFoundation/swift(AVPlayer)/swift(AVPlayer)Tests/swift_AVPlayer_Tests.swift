7c57cd30f5993fb1cf9e30468e57af6dc1b24618
786452470 <fyf786452470@gmail.com>
2018-08-29 09:18:57 +0800

    初始化swift(dispatchQueue), swift(CoreBlueTooth),swift(AVPlayer)

---
+++ swift4.2/frameworks(框架)/MediaLayer(媒体层)/AVFoundation/swift(AVPlayer)/swift(AVPlayer)Tests/swift_AVPlayer_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_AVPlayer_Tests.swift
+//  swift(AVPlayer)Tests
+//
+//  Created by 范云飞 on 2018/8/29.
+//  Copyright © 2018年 范云飞. All rights reserved.
+//
+
+import XCTest
+@testable import swift_AVPlayer_
+
+class swift_AVPlayer_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
